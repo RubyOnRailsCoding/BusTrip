6d4da0801f82dd0f1081d224dd0cc0bd87df673f
Mykola Matsko <mykola.matsko@gmail.com>
2017-05-10 23:24:07 +0300

    Base devise gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+	devise_scope :user do
+		get 'sign_in', to: 'devise/sessions#new'
+		get 'sign_up', to: 'devise/registrations#new'
+	end
   root 'trips#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
